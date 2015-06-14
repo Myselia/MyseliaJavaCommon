4f99d5a4b18d27bdc6b74b6176e0c7d9e3216e2a
TheOrangeDuke <ed.orangeduke@gmail.com>
2015-06-14 15:47:09 -0400

    UUID generation change

--- src/main/java/com/myselia/javacommon/topology/MyseliaUUID.java
+++ src/main/java/com/myselia/javacommon/topology/MyseliaUUID.java
@@ -2,14 +2,15 @@ package com.myselia.javacommon.topology;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import com.myselia.javacommon.constants.opcode.ComponentType;
 import com.myselia.javacommon.constants.opcode.OpcodeSegment;
 
 public class MyseliaUUID implements OpcodeSegment {
 	
 	private final String UUID;
 	
-	public MyseliaUUID(String host, String ip, String mac, String ct, String unixts){
-		this.UUID = DigestUtils.sha1Hex(host + ip + mac + ct + unixts);	
+	public MyseliaUUID(String host, String ip, String mac, ComponentType ct, String unixts){
+		this.UUID = DigestUtils.sha1Hex(host + ip + mac + ct.toString() + unixts);	
 	}
 	
 	public MyseliaUUID(String UUID){
